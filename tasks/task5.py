d03715cf15985232b375d7168896d3af85544ae6
Ирина Щиголева <sashahigolev@mail.ru>
2025-11-11 13:40:35 +0500

    fsafsafsaf

--- tasks/task5.py
+++ tasks/task5.py
@@ -2,7 +2,11 @@
 
 def solve():
 # Ниже пишите решение задачи
-    
+    a,b,c=map(str, input().split())
+    ord1=ord(a)
+    ord2=ord(b)
+    ord3=ord(c)
+    print("Код символа " + str(a) + " равен " + str(ord1) + '\n' + "Код символа " + str(b) + " равен " + str(ord2) + '\n'+ "Код символа " + str(c) + " равен " + str(ord3))
    
 # Код ниже не трогать! он нужен для тестов
 if __name__ == "__main__":
